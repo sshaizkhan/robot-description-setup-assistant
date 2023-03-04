d6e25efe9cff0c4700514b0a733637d3b0ae520b
Shahwaz Khan <sshaizkhan@gmail.com>
2023-03-04 15:02:35 -0500

    (add): python based robot_description_setup_assistant package

---
+++ robot_description_setup_assistant/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'robot_description_setup_assistant'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Shahwaz Khan',
+    maintainer_email='sshaizkhan@gmail.com',
+    description='PyQt5 based gui to create robot descriptions with custom end-effector tools',
+    license='BSD-Clause-3',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
