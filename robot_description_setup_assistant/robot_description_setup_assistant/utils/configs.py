b48d844fda7d30c67c05c7abf193e8c845664f91
Shahwaz Khan <sshaizkhan@gmail.com>
2023-03-05 17:11:35 -0500

    (modify): Joint and Kinematics config class

--- robot_description_setup_assistant/robot_description_setup_assistant/utils/configs.py
+++ robot_description_setup_assistant/robot_description_setup_assistant/utils/configs.py
@@ -1,9 +1,8 @@
-import yaml
+import math
 import os
-import json
+from dataclasses import dataclass, fields
 
-from dataclasses import dataclass, asdict, fields
-from typing import List, Dict
+import yaml
 
 
 @dataclass
@@ -36,6 +35,28 @@ class Kinematics:
     wrist_3: KinematicComponent
 
 
+@dataclass
+class JointParameters:
+    has_acceleration_limits: bool
+    has_effort_limits: bool
+    has_position_limits: bool
+    has_velocity_limits: bool
+    max_effort: float
+    max_position: float
+    max_velocity: float
+    min_position: float
+
+
+@dataclass
+class JointLimits:
+    shoulder: JointParameters
+    shoulder_lift: JointParameters
+    elbow_joint: JointParameters
+    wrist_1: JointParameters
+    wrist_2: JointParameters
+    wrist_3: JointParameters
+
+
 def dataclass_from_dict(klass, d):
     try:
         fieldtypes = {f.name: f.type for f in fields(klass)}
@@ -45,31 +66,47 @@ def dataclass_from_dict(klass, d):
 
 
 def create_kinematics_from_yaml(default_kinematics_path: str) -> Kinematics:
-
-    kinematic_config_dict: dict
     with open(default_kinematics_path, "r") as yaml_file:
         kinematic_config_dict = yaml.safe_load(yaml_file)
 
-    print(kinematic_config_dict)
-    print("=======================================")
-
-    components = [
+    kinematic_components = [
         KinematicComponent(
-            dataclass_from_dict(
-                Position, {"x": data["x"], "y": data["y"], "z": data["z"]}
-            ),
-            dataclass_from_dict(
-                Orientation,
-                {"roll": data["roll"], "pitch": data["pitch"], "yaw": data["yaw"]},
-            ),
+            dataclass_from_dict(Position, pose),
+            dataclass_from_dict(Orientation, pose),
         )
-        for data in kinematic_config_dict.values()
+        for kinematic in kinematic_config_dict.values()
+        for pose in kinematic.values()
     ]
 
-    # Create a Kinematics object using the components list as arguments
-    kinematics = Kinematics(*components)
+    kinematics = Kinematics(*kinematic_components)
     return kinematics
 
+
+def create_joint_limits_from_yaml(joint_limits_config_path: str) -> JointLimits:
+    with open(joint_limits_config_path, "r") as yaml_file:
+        joint_limits_config_dict = yaml.safe_load(yaml_file)
+
+    joint_params_list = [
+        dataclass_from_dict(
+            JointParameters,
+            {
+                "has_acceleration_limits": joint_params["has_acceleration_limits"],
+                "has_effort_limits": joint_params["has_effort_limits"],
+                "has_position_limits": joint_params["has_position_limits"],
+                "has_velocity_limits": joint_params["has_velocity_limits"],
+                "max_effort": joint_params["max_effort"],
+                "max_position": math.radians(joint_params["max_position"]),
+                "max_velocity": math.radians(joint_params["min_position"]),
+                "min_position": math.radians(joint_params["max_velocity"]),
+            },
+        )
+        for joint_data in joint_limits_config_dict.values()
+        for joint_params in joint_data.values()
+    ]
+
+    joint_limits = JointLimits(*joint_params_list)
+
+    return joint_limits
 
 if __name__ == "__main__":
     current_file_path = os.path.abspath(__file__)
@@ -89,5 +126,17 @@ if __name__ == "__main__":
     )
     a = create_kinematics_from_yaml(default_kinematics_path)
 
-    print(a.shoulder)
-    # print(type(a))
+    joint_limits_config_path = os.path.abspath(
+        os.path.join(
+            current_file_path,
+            "..",
+            "..",
+            "..",
+            "..",
+            "robot_description_resources",
+            "config",
+            "ur5",
+            "joint_limits.yaml",
+        )
+    )
+    b = create_joint_limits_from_yaml(joint_limits_config_path)
