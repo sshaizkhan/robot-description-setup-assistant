41e08199b0556410baa9c3c9cc6671cd0e18f76a
Shahwaz <shahwaz.khan@lab0.com>
2024-02-06 22:41:56 -0800

    Add start screen functionality to robot description core plugins

--- robot_description_core_plugins/src/start_screen_widget.cpp
+++ robot_description_core_plugins/src/start_screen_widget.cpp
@@ -1,13 +1,44 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2012, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+/* Author: Shahwaz Khan */
+/* Modified from original code by Dave Coleman */
+
 #include "robot_description_core_plugins/start_screen_widget.hpp"
 #include "robot_description_setup_framework/utilities.hpp"
 
 namespace robot_description::core_plugins
 {
-
-core_plugins::StartScreenWidget::StartScreenWidget(QWidget* parent) : QWidget(parent)
-{
-  onInit();
-}
 
 void StartScreenWidget::onInit()
 {
@@ -252,3 +283,7 @@ SelectModeWidget::SelectModeWidget(QWidget* parent) : QFrame(parent)
 }
 
 }  // namespace robot_description::core_plugins
+
+#include <pluginlib/class_list_macros.hpp>  // NOLINT
+PLUGINLIB_EXPORT_CLASS(robot_description::core_plugins::StartScreenWidget,
+                       robot_description::setup_framework::SetupStepWidget)
