f00d3825554cb9b1cb02f8d8c07ced99c5a6c416
Shahwaz <shahwaz.khan@lab0.com>
2024-02-08 11:06:34 -0800

    Add author(s) info

--- robot_description_setup_assistant/include/robot_description_setup_assistant/setup_assistant_widget.hpp
+++ robot_description_setup_assistant/include/robot_description_setup_assistant/setup_assistant_widget.hpp
@@ -31,6 +31,8 @@
  *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  *********************************************************************/
+/*Author: Shahwaz Khan*/
+/*Modified from the origianl code by Dave Coleman*/
 
 #pragma once
 
